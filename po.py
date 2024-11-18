6735b563d42410cd5976b25c38813eaae73eaa50
arnaldorocha <arnaldorochafilho@gmail.com>
2024-11-18 16:30:49 -0300

    update

--- po.py
+++
@@ -1,7 +0,0 @@
-# Pontuações manualmente usando range
-pontuacoes = ''.join(chr(i) for i in range(33, 48)) + \
-             ''.join(chr(i) for i in range(58, 65)) + \
-             ''.join(chr(i) for i in range(91, 97)) + \
-             ''.join(chr(i) for i in range(123, 127))
-
-print("Pontuações:", pontuacoes)
