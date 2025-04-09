6e149a6dc976d2c5cec842941cff86a0a6e93807
ieqstacruz <arnaldorochafilho@gmail.com>
2025-04-09 20:06:46 -0300

    Day 9

--- begginner/9_dictionary,_nesting_and_the_secret_auction/projeto/leilao.py
+++ begginner/9_dictionary,_nesting_and_the_secret_auction/projeto/leilao.py
@@ -1,3 +1,25 @@
+
+# TODO -1: Ask  the user for input
+# TODO -2: Save data into dictionary {name: price}
+# TODO -3: Whether if new bids need to be added
+# TODO -4: Compare bids in dictionary
+
+print(r'''              
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\
+                         `'-------'`
+                       .-------------.
+                      /_______________\      
+      
+                                                     ''')
+
 # Imprime uma mensagem de boas-vindas ao usuário
 print("Welcome to the secret auction program.\n")
 
